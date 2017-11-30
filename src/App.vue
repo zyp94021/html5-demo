67f5860882a3a5459902904834c091471c7be776
ZYP <zyp94021@gmail.com>
2017-11-30 16:23:59 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <sidebar></sidebar>
+    <div id="content">
+      <p>内容</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar.vue";
+
+export default {
+  components: {Sidebar},
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  #app{
+    #content{
+      position: absolute;
+      top:0px;
+      right: 0px;
+      left:350px;
+    }
+  }
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,91 @@
+<template>
+  <aside id="sidebar">
+    <ul>
+      <li v-for="data in nav">
+        <router-link :to="data.href">{{data.name}}</router-link>
+        <ul>
+          <li v-for="item in data.items">
+            <router-link :to="data.href+item.hash">{{item.name}}</router-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
+</template>
+<script>
+  export default {
+    name: 'Sidebar',
+    data() {
+      return {
+        nav: [
+          {
+            name: '视频',
+            href: '/video',
+            items: [
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              }
+            ]
+          },
+          {
+            name: '视频',
+            href: '/video',
+            items: [
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              }
+            ]
+          },
+          {
+            name: '视频',
+            href: '/video',
+            items: [
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              },
+              {
+                name: '标签',
+                hash:'#tag'
+              }
+            ]
+          },
+        ]
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  #sidebar {
+    position: fixed;
+    top:0px;
+    bottom:0px;
+    width: 340px;
+    overflow-y: auto;
+    ul {
+      list-style: none;
+    }
+  }
+</style>
